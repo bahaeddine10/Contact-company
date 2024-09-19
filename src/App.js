e230dc41164b6eeec6b1e07c9aff7cbad1bd9e6c
bahaeddine10 <75793407+bahaeddine10@users.noreply.github.com>
2024-09-19 12:40:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
         <Route path="services" element={<Services/>} />
         <Route path="careers" element={<Career/>} />
         
-        <Route path="*" element={<div>Not Found</div>} />
+        <Route path="*" element={<Home/>} />
 
       </Routes> 
     </BrowserRouter> 
