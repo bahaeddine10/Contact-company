e5166d771c211763f79e101fa0204784fe5a8eb0
baha eddine gassar <bahagassar@gmail.com>
2024-09-19 22:57:34 +0200

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,17 +18,17 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="m-auto">
-            <Nav.Link href="/Contact-company/">Home</Nav.Link>
-            <Nav.Link href="/Contact-company/about">About us</Nav.Link>
+            <Nav.Link href="../Contact-company/">Home</Nav.Link>
+            <Nav.Link href="../Contact-company/about">About us</Nav.Link>
             <NavDropdown title="À Propos" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/Contact-company/PC">À Propos de la Contact</NavDropdown.Item>
-              <NavDropdown.Item href="/Contact-company/NF">Nos Clients</NavDropdown.Item>
-              <NavDropdown.Item href="/Contact-company/QE">Qualité & Environnement</NavDropdown.Item>
+              <NavDropdown.Item href="../Contact-company/PC">À Propos de la Contact</NavDropdown.Item>
+              <NavDropdown.Item href="../Contact-company/NF">Nos Clients</NavDropdown.Item>
+              <NavDropdown.Item href="../Contact-company/QE">Qualité & Environnement</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/Contact-company/services">Services</Nav.Link>
-            <Nav.Link href="/Contact-company/media">Media</Nav.Link>
-            <Nav.Link href="/Contact-company/careers">Carrer</Nav.Link>
-            <Nav.Link href="/Contact-company/contact">Contact</Nav.Link>
+            <Nav.Link href="../Contact-company/services">Services</Nav.Link>
+            <Nav.Link href="../Contact-company/media">Media</Nav.Link>
+            <Nav.Link href="../Contact-company/careers">Carrer</Nav.Link>
+            <Nav.Link href="../Contact-company/contact">Contact</Nav.Link>
           </Nav>
           
         </Navbar.Collapse>
