baa36d76811f971d8c43669ba01d8c02fd214c43
baha eddine gassar <bahagassar@gmail.com>
2024-09-19 23:53:58 +0200

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,9 +6,9 @@ function NavBar() {
   return (
     <Navbar variant='top'  className='navbar' style={{backgroundColor: 'whitesmoke',position: 'sticky',top: '0',zIndex: '1'}}   expand="lg" >
       <Container>
-        <Navbar.Brand href="/Contact-company/">
+        <Navbar.Brand href="/Contact-company/#/">
           <img
-            src="../Contact-company/assets/img/contact.png"
+            src="./Contact-company/assets/img/contact.png"
             width="150"
             height="30"
             className="d-inline-block align-top"
@@ -18,17 +18,17 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="m-auto">
-            <Nav.Link href="/Contact-company/home">Home</Nav.Link>
-            <Nav.Link href="/Contact-company/about">About us</Nav.Link>
+            <Nav.Link href="/Contact-company/">Home</Nav.Link>
+            <Nav.Link href="/Contact-company/#/about">About us</Nav.Link>
             <NavDropdown title="À Propos" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/Contact-company/PC">À Propos de la Contact</NavDropdown.Item>
-              <NavDropdown.Item href="/Contact-company/NF">Nos Clients</NavDropdown.Item>
-              <NavDropdown.Item href="/Contact-company/QE">Qualité & Environnement</NavDropdown.Item>
+              <NavDropdown.Item href="/Contact-company/#/PC">À Propos de la Contact</NavDropdown.Item>
+              <NavDropdown.Item href="/Contact-company/#/NF">Nos Clients</NavDropdown.Item>
+              <NavDropdown.Item href="/Contact-company/#/QE">Qualité & Environnement</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/Contact-company/services">Services</Nav.Link>
-            <Nav.Link href="/Contact-company/media">Media</Nav.Link>
-            <Nav.Link href="/Contact-company/careers">Carrer</Nav.Link>
-            <Nav.Link href="/Contact-company/contact">Contact</Nav.Link>
+            <Nav.Link href="/Contact-company/#/services">Services</Nav.Link>
+            <Nav.Link href="/Contact-company/#/media">Media</Nav.Link>
+            <Nav.Link href="/Contact-company/#/careers">Carrer</Nav.Link>
+            <Nav.Link href="/Contact-company/#/contact">Contact</Nav.Link>
           </Nav>
           
         </Navbar.Collapse>
