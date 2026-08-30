ba82883cb2609ff04be5fd2ae9a3cc98d7353240
ademAbassi <aabassi@mindstate.tech>
2026-08-30 14:33:57 +0100

    fea : navbar change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,44 +1,44 @@
 import React from 'react';
-import { Navbar, Container, Nav , Button} from 'react-bootstrap';
-
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import './NavBar.css';
 
 function NavBar() {
   return (
-    <Navbar variant='top'  className='navbar' style={{backgroundColor: 'whitesmoke',position: 'sticky',top: '0',zIndex: '1'}}   expand="lg" >
+    <Navbar expand="lg" sticky="top" className="custom-navbar py-3">
       <Container>
-        <Navbar.Brand href="/Contact-company/">
+        <Navbar.Brand href="#/" className="d-flex align-items-center">
           <img
             src="/assets/img/Contact.png"
-            width="150"
-            height="30"
-            className="d-inline-block align-top"
-            alt="Contact logo"
+            height="36"
+            className="d-inline-block align-top brand-logo"
+            alt="Company logo"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="m-auto">
-            <Nav.Link href="#/">Home</Nav.Link>
-            <Nav.Link href="#/about">About us</Nav.Link>
-            
-            <Nav.Link href="#/services">Services</Nav.Link>
-            <Nav.Link href="#/Apply">Apply</Nav.Link>
+        
+        <Navbar.Toggle aria-controls="main-navbar-nav" className="border-0 shadow-none px-0" />
 
-            <Nav.Link href="#/careers">Carrer</Nav.Link>
-            <Nav.Link href="#/contact">Contact</Nav.Link>
-           
-
+        <Navbar.Collapse id="main-navbar-nav">
+          <Nav className="mx-auto align-items-center gap-lg-3 my-3 my-lg-0">
+            <Nav.Link href="#/" className="nav-link-custom">Home</Nav.Link>
+            <Nav.Link href="#/about" className="nav-link-custom">About Us</Nav.Link>
+            <Nav.Link href="#/services" className="nav-link-custom">Services</Nav.Link>
+            <Nav.Link href="#/apply" className="nav-link-custom">Apply</Nav.Link>
+            <Nav.Link href="#/careers" className="nav-link-custom">Careers</Nav.Link>
+            <Nav.Link href="#/contact" className="nav-link-custom">Contact</Nav.Link>
           </Nav>
-          <Button variant="primary" onClick={() => window.location.href = '#/login'}>
-          Login
-        </Button>
-         {/*<Button variant="primary" onClick={() => window.location.href = '#/signup'}>
-          sign up
-        </Button>*/}
+
+          <div className="d-flex align-items-center gap-2">
+            <Button 
+              className="btn-modern px-4 py-2"
+              onClick={() => window.location.href = '#/login'}
+            >
+              Log In
+            </Button>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default NavBar; 
+export default NavBar;
